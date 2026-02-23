74ea07c7e18589dce5b27364004aea18394bab32
Menna Wagih <130869533+Mennawagih@users.noreply.github.com>
2026-02-23 21:33:58 +0200

    api.py

--- api.py
+++ api.py
@@ -229,5 +229,12 @@ def api_classify_model2():
 # ---------------------------------------------------------
 # RUN SERVER
 # ---------------------------------------------------------
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=7860, debug=True)
+
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=7860, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    
